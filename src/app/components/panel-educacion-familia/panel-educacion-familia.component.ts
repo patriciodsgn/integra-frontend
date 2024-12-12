9293ef904f44654aa0f12fea4e6f3460994ee858
patricio-dsgn <patricio.ngg2@gmail.com>
2024-12-12 08:23:00 -0300

    fusion 5.2

---
+++ src/app/components/panel-educacion-familia/panel-educacion-familia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-educacion-familia',
+  standalone: true,
+  imports: [],
+  templateUrl: './panel-educacion-familia.component.html',
+  styleUrl: './panel-educacion-familia.component.css'
+})
+export class PanelEducacionFamiliaComponent {
+
+}
